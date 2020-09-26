93d109b9fab0985a8620afcf2d450315063a7671
Ekaterina <scripts@robotess.net>
2020-09-26 20:01:15 +0200

    Test for #42 + fixes

--- tests/acceptance/simpleui/CheckUICest.php
+++ tests/acceptance/simpleui/CheckUICest.php
@@ -31,10 +31,11 @@ class CheckUICest
         $I->see('Welcome to the Enthusiast [Robotess Fork] v. 1.0.5 admin panel for My Collective!', 'h1');
     }
 
-    public function seeCorrectOpenedDateInStatsWidget(AcceptanceTester $I): void
+    public function seeCorrectDatesInStatsWidget(AcceptanceTester $I): void
     {
         $I->amOnPage('/samplefl');
         $I->see('Opened: 17th February 2010');
+        $I->see('Last updated: 24th June 2019');
     }
 
     public function seeCorrectLinkToLostpassPage(AcceptanceTester $I): void
